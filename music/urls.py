a3fe5aba1674e90937bbcb371932da9332abefa1
Your Name <you@example.com>
2017-01-06 23:22:17 +0530

    create index and base files

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from . import views
 
 app_name = 'music'
 
 urlpatterns = [
-
+    url(r'^index/$' ,views.index , name='index' ),
+    url(r'' , views.index , name='index'),
 ]
